2b3ddb02533054d3388a2ded9b95969ea73cb32f
sw.yoon <zoomspeed77@gmail.com>
2025-04-06 17:47:51 +0900

    feat: API 요청 유효성 검증 추가

--- src/main/java/com/musinsa/codi/common/exception/ErrorCode.java
+++ src/main/java/com/musinsa/codi/common/exception/ErrorCode.java
@@ -8,6 +8,7 @@ public enum ErrorCode {
     BRAND_NOT_FOUND(HttpStatus.NOT_FOUND, "브랜드를 찾을 수 없습니다."),
     BRAND_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 브랜드입니다."),
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
+    PRODUCT_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 상품입니다."),
     CODI_NOT_FOUND(HttpStatus.NOT_FOUND, "코디를 찾을 수 없습니다."),
     INVALID_PRICE(HttpStatus.BAD_REQUEST, "유효하지 않은 가격입니다."),
     INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "유효하지 않은 카테고리입니다.");
